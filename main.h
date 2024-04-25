e4f2d58f51310eeb2714b9a0ec795955d0c84c8b
Doctware <yusclever9@gmail.com>
2024-04-25 01:34:12 +0100

    The header file

--- main.h
+++ main.h
@@ -4,16 +4,10 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <limits.h>
 
-int _putchar(char c);
+/* Prototypes */
 int _printf(const char *format, ...);
-int print_number(int n);
-int dec_to_binary(int n);
-int _putstring(char *s);
-void _reverse(char *s);
-int rev_str(char *s);
-int rot13(char *point);
-int print_digit(int n);
-int _strlen(char *s);
+int _putchar(char c);
 
 #endif
